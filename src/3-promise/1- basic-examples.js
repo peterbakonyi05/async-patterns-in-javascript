2075a763ba3a824a029bea8bfd1b6f71db511834
BETSSON\peba02 <peter.bakonyi@betssongroup.com>
2017-08-08 20:09:42 +0200

    promise: add basic example

--- src/3-promise/1- basic-examples.js
+++ src/3-promise/1- basic-examples.js
@@ -1,3 +1,33 @@
 // promise: pending, fulfilled, rejected
 // if it is fulfilled or rejected, it means it is settled
 
+describe("promise", () => {
+    describe("resolving a promise", () => {
+        it("should call the success callback (always!) async", done => {
+            let isAsync = false;
+            Promise.resolve(1).then(data => {
+                expect(data).toBe(1);
+                expect(isAsync).toBe(true);
+                done();
+            });
+            isAsync = true;
+        });
+    });
+
+    describe("chaining", () => {
+        it("should allow chaining multiple promises", done => {
+            Promise.resolve(1)
+                .then(i => i * 2)
+                .then(i => {
+                    expect(i).toBe(2);
+                    // returning a new promise
+                    return Promise.resolve(i * 2);
+                })
+                .then(value => {
+                    // here it is the value
+                    expect(value).toBe(4);
+                    done();
+                });
+        });
+    });
+});
