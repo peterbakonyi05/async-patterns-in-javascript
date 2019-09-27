73161e1e96c882e8c63cc09bffe39c1a8eef3119
BETSSON\peba02 <peter.bakonyi@betssongroup.com>
2019-09-27 10:32:35 +0200

    update

--- src/3-promise/2-parallel-execution.js
+++ src/3-promise/2-parallel-execution.js
@@ -1,1 +1,23 @@
-// todo
+function readFile(path) {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(`${path} content`), 0);
+    });
+}
+
+describe("promise", () => {
+    describe("parallel execution", () => {
+        it("should run promises code", done => {
+            Promise.all([
+                readFile("a.txt"),
+                readFile("b.txt")
+            ])
+                .then(data => {
+                    expect(data).toEqual([
+                        "a.txt content",
+                        "b.txt content"
+                    ]);
+                    done();
+                });
+        });
+    });
+})
